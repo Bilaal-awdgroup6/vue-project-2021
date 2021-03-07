898eab26a8e7fa8bab19e436a1168a5fca813d79
Bilaal-awdgroup6 <bilaal.awdgroup6@gmail.com>
2021-03-07 18:40:52 +0000

    Updating Firebase Config
    Updating the config so that it connects properly with my new individual firebase account

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -4,14 +4,13 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBWx4p0j16mdUIivZLBqz_8l_BH6OhRpR4",
-    authDomain: "myocardio-awdg6.firebaseapp.com",
-    projectId: "myocardio-awdg6",
-    storageBucket: "myocardio-awdg6.appspot.com",
-    messagingSenderId: "886784373926",
-    appId: "1:886784373926:web:08305df28d2701e4c3b067",
-    measurementId: "G-PK42GS9GFN"
-  };
+  apiKey: "AIzaSyBlU7cNuVX1iB9dhDRvUY2IcaCi4-b77Do",
+  authDomain: "bilaal-myocardio-awdg6-8e63a.firebaseapp.com",
+  projectId: "bilaal-myocardio-awdg6-8e63a",
+  storageBucket: "bilaal-myocardio-awdg6-8e63a.appspot.com",
+  messagingSenderId: "137865187451",
+  appId: "1:137865187451:web:d2f7f68b8dda5478ca92de"
+};
 
   //initialize firebase
   firebase.initializeApp(firebaseConfig);
